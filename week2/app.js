a65a1cddaf90e698244f508e528acc90ae5a1de6
Tuomas01 <tuomastheikkila@hotmail.fi>
2022-11-01 15:02:05 +0200

    adds dummy routing

--- week2/app.js
+++ week2/app.js
@@ -4,7 +4,25 @@ const app = express();
 const port = 3000;
 
 app.get('/cat', (req, res) => {
+  //console.log(req); // <- req has all the data about the request
   res.send('From this endpoint you can get cats.')
+});
+
+app.post('/cat', (req, res) => {
+  res.send('From this endpoint you can add more cats.');
+});
+
+app.get('/cat/:catId', (req, res) => {
+  console.log(req.params);
+  res.send('From this endpoint you can a cat with an id: ' + req.params.catId);
+});
+
+app.put('/cat', (req, res) => {
+  res.send('From this endpoint you can edit cats');
+});
+
+app.delete('/cat', (req, res) => {
+  res.send('From this endpoint you can delete cats');
 });
 
 app.get('/user', (req, res) => {
