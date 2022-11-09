893d60373a3684f41570ba3e1e6bc120da5561ed
Tuomas01 <tuomastheikkila@hotmail.fi>
2022-11-09 10:53:40 +0200

    adds user endpoints

--- week2/controllers/userController.js
+++ week2/controllers/userController.js
@@ -23,8 +23,27 @@ const createUser = async (req, res) => {
   res.status(201).json({userId: result});
 };
 
-const modifyUser = (req, res) => {};
-const deleteUser = (req, res) => {};
+const modifyUser = async (req, res) => {
+  const user = req.body;
+  if (req.param.userId) {
+    user.id = req.params.userId;
+  }
+  const result = await userModel.updateUserById(user, res);
+  if (result.affectedRows > 0) {
+    res.json({message: "user updated: " + user.id});
+  } else {
+    res.status(404).json({message: 'nothing was changed'});
+  }
+};
+const deleteUser = async (req, res) => {
+  const result = await userModel.deleteUserById(req.params.userId, res);
+  console.log("user delted", result);
+  if (result.affectedRows > 0) {
+    res.json({message: "user deleted"});
+  } else {
+    res.status(404).json({message: 'user not found'});
+  }
+};
 
 module.exports = {
   getUser,
--- week2/routes/userRoute.js
+++ week2/routes/userRoute.js
@@ -3,14 +3,11 @@ const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/userController');
 
-router.get('/', userController.getUsers);
-
-router.get('/:userId', userController.getUser);
-
-router.post('/', userController.createUser);
-
-router.put('/', userController.modifyUser);
-
-router.delete('/', userController.deleteUser);
+router.get('/', userController.getUsers)
+.get('/:userId', userController.getUser)
+.post('/', userController.createUser)
+.put('/', userController.modifyUser)
+.put('/:userId', userController.modifyUser)
+.delete('/:catId', userController.deleteUser);
 
 module.exports = router;
